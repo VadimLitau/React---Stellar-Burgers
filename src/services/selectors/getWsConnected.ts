e82abd301b9702c3208f855b72893dc8c7b6c70a
VadimLitau <vadimlitau@yandex.ru>
2022-09-21 21:58:42 +0600

    Updates

--- src/services/selectors/getWsConnected.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from "../types";
-
-export const getWsConnected = (state: RootState) => state.ws.wsConnected;
--- src/services/selectors/wsSelectors.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "../types";
-
-export const getMessages = (store: RootState) => store.ws.messages || [];
-export const getUser = (store: RootState) => store.route.userAuthProfile;
